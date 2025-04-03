cdedd7c9ea3ddfaf5308152d3b1d5b1ac9706a5e
onyi-e <message.onyi@gmail.com>
2025-04-03 17:05:23 +0100

    updated javascript with responsive search bar built using weather API

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ function userInput(event) {
   let searchInput = document.querySelector(".input-field");
   let h1 = document.querySelector("h1");
   h1.innerHTML = searchInput.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${h1.innerHTML}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
 }
+function displayWeather(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+  let featureCityTemp = document.querySelector(".featured-temp");
+  let roundedUpTemp = Math.round(response.data.temperature.current);
+  featureCityTemp.innerHTML = `${roundedUpTemp}`;
+
+  console.log(response.data.city);
+}
+let apiKey = "3aco6795a94b0838a7e43f73ad5b4e0t";
 
 function currentDate(date) {
   let days = [
@@ -32,6 +44,6 @@ inputField.addEventListener("submit", userInput);
 
 let updateddate = new Date();
 
-let cityDate = document.querySelector("#city-date");
+let cityDate = document.querySelector("#city");
 
 cityDate.innerHTML = currentDate(updateddate);
